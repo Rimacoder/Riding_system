1e1467c7b3cc74ea3e575d7db6921635538e50e2
Rima Maurya <114494799+Rimacoder@users.noreply.github.com>
2026-04-10 19:44:12 +0530

    Create AuthResponse.java

---
+++ src/main/java/com/ridingsystem/dto/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.ridingsystem.dto;
+
+import com.ridingsystem.entity.Role;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthResponse {
+    private String token;
+    private Long userId;
+    private String name;
+    private Role role;
+}
