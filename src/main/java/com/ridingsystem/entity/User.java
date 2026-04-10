85a20285314cf5610bed17d1295830b8e1c41b35
Rima Maurya <114494799+Rimacoder@users.noreply.github.com>
2026-04-10 21:12:40 +0530

    Create User.java

---
+++ src/main/java/com/ridingsystem/entity/User.java
@@ -0,0 +1,44 @@
+package com.ridingsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    private String vehicleModel;
+    private String licensePlate;
+    private Integer vehicleCapacity;
+
+    @OneToMany(mappedBy = "driver", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Ride> postedRides = new ArrayList<>();
+}
