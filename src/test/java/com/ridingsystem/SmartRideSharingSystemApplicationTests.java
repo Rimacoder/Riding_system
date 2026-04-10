3186bc8f411f6a5a6d2be53834bf921725d1395e
Rima Maurya <114494799+Rimacoder@users.noreply.github.com>
2026-04-10 21:28:29 +0530

    Create SmartRideSharingSystemApplicationTests.java

---
+++ src/test/java/com/ridingsystem/SmartRideSharingSystemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ridingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRideSharingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
