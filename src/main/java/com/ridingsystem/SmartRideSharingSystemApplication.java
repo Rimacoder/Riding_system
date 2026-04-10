800afb2f4c63e32ee52b5201792dfe3701708026
Rima Maurya <114494799+Rimacoder@users.noreply.github.com>
2026-04-10 19:19:53 +0530

    Create SmartRideSharingSystemApplication.java

---
+++ src/main/java/com/ridingsystem/SmartRideSharingSystemApplication.java
@@ -0,0 +1,12 @@
+package com.ridingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartRideSharingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartRideSharingSystemApplication.class, args);
+    }
+}
